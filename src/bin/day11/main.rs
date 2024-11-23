d86d5e941d6ff2ddef2cb38f4d8e6c5cba090968
Alexis Delhelle <adelhelle@hellowork.com>
2024-11-23 12:44:44 +0100

    init repo

---
+++ src/bin/day11/main.rs
@@ -0,0 +1,20 @@
+use std::time::Instant;
+
+fn main() {
+    let input = include_str!("input.txt");
+    let start = Instant::now();
+    part1(input);
+    println!("day1 took : {:?}", start.elapsed());
+
+    let start = Instant::now();
+    part2(input);
+    println!("day2 took : {:?}", start.elapsed());
+}
+
+fn part1(input: &str) {
+    println!("{:?}", input);
+}
+
+fn part2(input: &str) {
+    println!("{:?}", input);
+}
