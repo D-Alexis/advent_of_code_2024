309d8a320d4a39a8f742570f4474f807bca1d7cd
Alexis Delhelle <adelhelle@hellowork.com>
2024-12-01 10:44:26 +0100

    day1

--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -1,4 +1,4 @@
-use std::time::Instant;
+use std::{collections::HashMap, time::Instant};
 
 fn main() {
     let input = include_str!("input.txt");
@@ -12,9 +12,52 @@ fn main() {
 }
 
 fn part1(input: &str) {
-    println!("{:?}", input);
+    let (left, right) = parse_input(input);
+    let mut sum = 0;
+    for i in 0..left.len() {
+        sum += (left[i] - right[i]).abs();
+    }
+    println!("sum is : {:?}", sum);
 }
 
 fn part2(input: &str) {
-    println!("{:?}", input);
+    let mut left_vec = Vec::with_capacity(1000);
+    let mut right_vec = HashMap::with_capacity(1000);
+    input
+        .trim_end()
+        .split('\n')
+        .collect::<Vec<&str>>()
+        .iter()
+        .for_each(|line| {
+            let (left, right) = line.split_once("   ").unwrap();
+            let left = left.parse::<i32>().unwrap();
+            let right = right.parse::<i32>().unwrap();
+            left_vec.push(left);
+            *right_vec.entry(right).or_insert(0) += 1;
+        });
+    let a = left_vec
+        .iter()
+        .map(|left_value| left_value * right_vec.get(left_value).or_else(|| Some(&0)).unwrap())
+        .sum::<i32>();
+    println!("sum is : {:?}", a);
+}
+
+fn parse_input(input: &str) -> (Vec<i32>, Vec<i32>) {
+    let mut left_vec = Vec::with_capacity(1000);
+    let mut right_vec = Vec::with_capacity(1000);
+    input
+        .trim_end()
+        .split('\n')
+        .collect::<Vec<&str>>()
+        .iter()
+        .for_each(|line| {
+            let (left, right) = line.split_once("   ").unwrap();
+            let left = left.parse::<i32>().unwrap();
+            let right = right.parse::<i32>().unwrap();
+            left_vec.push(left);
+            right_vec.push(right);
+        });
+    left_vec.sort();
+    right_vec.sort();
+    (left_vec, right_vec)
 }
