da1a58da3e37f9551c6de12955694bbb59164afa
Alexis Delhelle <adelhelle@hellowork.com>
2024-12-03 08:08:37 +0100

    day3

--- src/bin/day3/main.rs
+++ src/bin/day3/main.rs
@@ -1,5 +1,5 @@
+use regex::Regex;
 use std::time::Instant;
-
 fn main() {
     let input = include_str!("input.txt");
     let start = Instant::now();
@@ -12,9 +12,28 @@ fn main() {
 }
 
 fn part1(input: &str) {
-    println!("{:?}", input);
+    let mut res = 0;
+    let rex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    rex.captures_iter(input).for_each(|matc| {
+        res += matc.get(1).unwrap().as_str().parse::<i32>().unwrap()
+            * matc.get(2).unwrap().as_str().parse::<i32>().unwrap();
+    });
+    println!("{:?}", res);
 }
 
 fn part2(input: &str) {
-    println!("{:?}", input);
+    let mut res = 0;
+    let mut donut = true;
+    let rex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|(do\(\)|don\'t\(\))").unwrap();
+    rex.captures_iter(input).for_each(|matc| {
+        // matc est sous la forme [matchTotal, group1, group2, group3] eg: [do(), None, None,do()] ou [mul(3,4), 3, 4, None]
+        let first = matc.get(0).unwrap().as_str();
+        if first == "do()" || first == "don't()" {
+            donut = first == "do()"
+        } else if donut {
+            res += matc.get(1).unwrap().as_str().parse::<i32>().unwrap()
+                * matc.get(2).unwrap().as_str().parse::<i32>().unwrap();
+        }
+    });
+    println!("{:?}", res);
 }
