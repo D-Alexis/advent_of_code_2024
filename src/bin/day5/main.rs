248ff76058790ca42b32e160318cc457b85486b7
Alexis Delhelle <adelhelle@hellowork.com>
2024-12-05 11:29:36 +0100

    en branchant la boite à cerveau c'est mieux

--- src/bin/day5/main.rs
+++ src/bin/day5/main.rs
@@ -34,53 +34,45 @@ fn part2(input: &str) {
             if is_ok {
                 0
             } else {
-                let mut has_changed = true;
-                while has_changed {
-                    has_changed = false;
-                    let length = update.len();
-                    for i in 0..length-1 {
-                        if let Some(list) = rules.get(&(update[i])) {
-                            if !list.contains(&(update[i] , update[i+1])) {
-                                let val = update[i];
-                                update[i] = update[i+1];
-                                update[i+1] = val;
-                                has_changed = true;
-                            }
-                        } else { // if it has no rules where it is first element (which is only in test and not in real data, fuck you btw)
-                            let val = update[i];
-                            update[i] = update[length-1];
-                            update[length-1] = val;
-                            has_changed = true;
-                        }
-                    }
-                }
-
-                update[update.len()/2] // (n/2)+1-1, oui toute les lignes sont impair
+                let mut result = 0;
+                let len_on_the_right = (update.len() / 2) as i32; // len/2 ->  if the len is 15, the middle element has 7 element to its right
+                for i in 0..update.len() {
+                   if let Some(list) = rules.get(&(update[i])) {
+                       let sum = update.iter().fold(0, |acc, val| {
+                           acc + if list.contains(val) {1} else {0}
+                       });
+                       if sum == len_on_the_right {
+                           result = update[i];
+                           break;
+                       }
+                   }
+               }
+               result
             }
         }).sum::<i32>());
 }
 
 
-fn check_if_valid(rules: &HashMap<i32, Vec<(i32, i32)>>, update: &Vec<i32>) -> bool {
+fn check_if_valid(rules: &HashMap<i32, Vec<i32>>, update: &Vec<i32>) -> bool {
     let mut is_ok = true;
     for i in 0..update.len() - 1 {
-        for j in i + 1..update.len() {
-            if let Some(list) = rules.get(&(update[i])) {
-                if !list.contains(&(update[i] , update[j])) {
-                    is_ok = false;
-                    break;
-                }
-            } else {
+        if let Some(list) = rules.get(&(update[i])) {
+            if !update[i+1..].iter().all(|a| {
+                list.contains(a)
+            }) {
                 is_ok = false;
                 break;
             }
+        } else {// if it has no rules where it is first element (which is only in test and not in real data, fuck you btw)
+            is_ok = false;
+            break;
         }
     }
     is_ok
 }
 
-fn parse_input(input: &str) -> (HashMap<i32, Vec<(i32, i32)>>, Vec<Vec<i32>>) {
-    let mut rules_map: HashMap<i32, Vec<(i32, i32)>> = HashMap::with_capacity(1200);
+fn parse_input(input: &str) -> (HashMap<i32, Vec<i32>>, Vec<Vec<i32>>) {
+    let mut rules_map: HashMap<i32, Vec<i32>> = HashMap::with_capacity(1200);
     let (first, second) = input.split_once("\n\n").unwrap();
     first.lines().for_each(|line| {
         let (left, right) = line.split_once('|').unwrap();
@@ -88,7 +80,7 @@ fn parse_input(input: &str) -> (HashMap<i32, Vec<(i32, i32)>>, Vec<Vec<i32>>) {
         rules_map
             .entry(left)
             .or_insert(Vec::with_capacity(15))
-            .push((left, right));
+            .push(right);
     });
     let updates = second
         .lines()
